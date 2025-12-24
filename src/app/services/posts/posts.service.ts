ca3b6cc26695e27cbfe045e3f7ab65d4baad674b
Jorys Pephily <jorys.pephily@coda-student.school>
2025-12-24 16:08:51 +0100

    affichage du nom des utilisateurs dans post

--- src/app/services/posts/posts.service.ts
+++ src/app/services/posts/posts.service.ts
@@ -6,17 +6,45 @@ import { Injectable } from "@angular/core";
 export class PostsService {
   constructor() {}
 
-  getAllPosts() {
-    return fetch("https://dummyjson.com/posts")
-      .then((response) => response.json())
-      .then((data) => data.posts);
+  async getAllPosts() {
+    const response = await fetch("https://dummyjson.com/posts");
+    const json = await response.json();
+
+    const postsWithUsers = await Promise.all(
+      json.posts.map(async (post: any) => {
+        const userResponse = await fetch(
+          `https://dummyjson.com/users/${post.userId}`,
+        );
+        const user = await userResponse.json();
+        console.log(user);
+
+        return {
+          ...post,
+          user,
+        };
+      }),
+    );
+
+    console.log(postsWithUsers);
+
+    return postsWithUsers;
   }
 
-  getPostById(id: number) {
-    return fetch(`https://dummyjson.com/posts/${id}`)
-      .then((response) => response.json())
-      .then((data) => data);
+  async getPostById(id: number) {
+    const postResponse = await fetch(`https://dummyjson.com/posts/${id}`);
+    const post = await postResponse.json();
+
+    const userResponse = await fetch(
+      `https://dummyjson.com/users/${post.userId}`,
+    );
+    const user = await userResponse.json();
+
+    return {
+      ...post,
+      user,
+    };
   }
+
   getCommentsByPostId(id: number) {
     return fetch(`https://dummyjson.com/post/${id}/comments`)
       .then((response) => response.json())
