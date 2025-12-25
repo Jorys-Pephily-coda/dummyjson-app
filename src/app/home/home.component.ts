2560251daf4de0553ecd17f071172e9a4a9c9600
Para <nathan.para@coda-student.school>
2025-12-25 20:32:49 +0100

    page home fini

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private router: Router) { }
+
+  redirectToUsers() {
+    this.router.navigate(['/users']);
+  }
+  redirectToPosts() {
+    this.router.navigate(['/posts']);
+  }
 
 }
